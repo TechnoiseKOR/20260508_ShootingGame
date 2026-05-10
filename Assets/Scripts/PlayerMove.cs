b177ed74ebd640a249e2d50a86a24b54c5869618
Technoise <technoisekor@gmail.com>
2026-05-11 02:05:39 +0900

    플레이어 이동시키기

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // 플레이어가 이동할 속력
+    public float speed = 5.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, v, 0.0f);
+        transform.position = transform.position + dir * speed * Time.deltaTime;
+    }
+}
