45e973e5c0a539a2958dbbac5782ce90eb4e6fff
Technoise <technoise.kor@gmail.com>
2026-05-13 16:23:35 +0900

    배경 추가 배경 머티리얼 생성 및 적용 배경 이동 구현

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    // 배경 머티리얼
+    public Material bgMaterial;
+
+    // 스크롤 속도
+    public float scrollSpeed = 0.2f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // 1. 살아 있는 동안 계속 하고 싶다.
+    // Update is called once per frame
+    void Update()
+    {
+        // 2. 방향이 필요하다.
+        Vector2 direction = Vector2.up;
+        // 3. 스크롤 하고 싶다. P = P0 + vt
+        bgMaterial.mainTextureOffset += direction * scrollSpeed * Time.deltaTime;
+    }
+}
