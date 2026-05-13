61a1e760e7b6880b55dc27440562518ba355127c
Technoise <technoise.kor@gmail.com>
2026-05-13 17:16:39 +0900

    현재점수, 최고점수 UI 추가
    현재 점수 증가 적용

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+// 유니티 UI를 사용하기 위한 네임스페이스
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    // 현재 점수 UI
+    public Text currentScoreUI;
+    // 현재 점수
+    public int currentScore;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
